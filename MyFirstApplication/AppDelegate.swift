0c2d31fa15a4c9922c59667afcf8130144bf6836
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2015-02-07 06:47:19 -0200

    Primeiro app finalizado

--- MyFirstApplication/AppDelegate.swift
+++ MyFirstApplication/AppDelegate.swift
@@ -13,7 +13,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
+    @IBOutlet weak var txtFirstName: NSTextField!
+    @IBOutlet weak var txtLastName: NSTextField!
+    @IBOutlet weak var txtFullName: NSTextField!
+    
+    var statusBar = NSStatusBar.systemStatusBar()
+    var statusBarItem : NSStatusItem = NSStatusItem()
+    var menu: NSMenu = NSMenu()
+    var menuItem : NSMenuItem = NSMenuItem()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
@@ -22,6 +30,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-
+    @IBAction func buttonConcatenate(sender: AnyObject) {
+        txtFullName.stringValue = txtFirstName.stringValue + " " + txtLastName.stringValue
+    }
 }
 
