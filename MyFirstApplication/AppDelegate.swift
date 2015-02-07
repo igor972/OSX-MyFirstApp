d8994e750da7aa94f1d3eaa2fe5c5f464cfd809f
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2015-02-07 05:52:49 -0200

    Initial Commit

---
+++ MyFirstApplication/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MyFirstApplication
+//
+//  Created by Igor Oliveira Antonio on 2/7/15.
+//  Copyright (c) 2015 Igor Oliveira Antonio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
